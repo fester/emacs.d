be95a0d5f5423f42becc83e39f019d1425de7fc5
Tim Baga <tim.baga@here.com>
2016-11-13 21:19:13 +0100

    Some order into the packages and used better-defaults

--- init.el
+++ init.el
@@ -1,19 +1,36 @@
+(defalias 'yes-or-no-p 'y-or-n-p)
+(setq mac-option-modifier 'super)
+(setq mac-command-modifier 'meta)
+
+;; no startup msg  
+(setq inhibit-startup-message t)        ; Disable startup message
+
+
 (require 'package)
 
 (setq package-archives
       '(("original"    . "http://tromey.com/elpa/")
         ("gnu"         . "http://elpa.gnu.org/packages/")
-        ("marmalade"   . "http://marmalade-repo.org/packages/")
-        ("melpa" . "http://melpa.milkbox.net/packages/")
+        ;; ("marmalade"   . "http://marmalade-repo.org/packages/")
+        ("melpa" . "http://stable.melpa.org/packages/")
         ("elpy" . "https://jorgenschaefer.github.io/packages/")))
+
 (package-initialize)
 
-(setq additional-packages 
-      '(magit moe-theme yasnippet yaml-mode yas-jit
-              scala-mode2 ensime
 
-              pyenv-mode elpy
-              markdown-mode))
+(setq additional-packages 
+      '(
+        better-defaults exec-path-from-shell
+
+        magit
+	moe-theme yasnippet yaml-mode yas-jit
+
+	pyenv-mode elpy
+	markdown-mode
+
+	clojure-mode cider projectile
+	paredit rainbow-delimiters
+	))
 
 ; fetch the list of packages available 
 (when (not package-archive-contents)
@@ -25,7 +42,9 @@
     (package-install package)))
 
 
-(defalias 'yes-or-no-p 'y-or-n-p)
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
 
 (load-theme 'moe-dark t)
 
@@ -47,28 +66,11 @@
 (package-initialize)
 (elpy-enable)
 
-(global-set-key (kbd "M-/") 'hippie-expand)
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-
-(global-set-key (kbd "C-s") 'isearch-forward-regexp)
-(global-set-key (kbd "C-r") 'isearch-backward-regexp)
-(global-set-key (kbd "C-M-s") 'isearch-forward)
-(global-set-key (kbd "C-M-r") 'isearch-backward)
-
 (global-set-key (kbd "C-c m") 'magit-status)
 
 (highlight-indentation-mode -1)
 
 (show-paren-mode 1)
-(setq-default indent-tabs-mode nil)
-(setq x-select-enable-clipboard t
-      x-select-enable-primary t
-      save-interprogram-paste-before-kill t
-      apropos-do-all t
-      mouse-yank-at-point t
-      save-place-file (concat user-emacs-directory "places")
-      backup-directory-alist `(("." . ,(concat user-emacs-directory
-                                               "backups"))))
 
 (setq auto-mode-alist
       (append
@@ -85,5 +87,13 @@
  ;; If there is more than one, they won't work right.
  '(elpy-modules
    (quote
-    (elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-yasnippet elpy-module-sane-defaults)))
- '(ns-command-modifier (quote meta)))
+    (elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-yasnippet elpy-module-sane-defaults)))
+ '(elpy-rpc-backend "jedi")
+ '(exec-path-from-shell-check-startup-files nil)
+ '(safe-local-variable-values
+   (quote
+    ((cider-cljs-lein-repl . "(do (dev) (go) (cljs-repl))")
+     (cider-refresh-after-fn . "reloaded.repl/resume")
+     (cider-refresh-before-fn . "reloaded.repl/suspend")))))
+
+(setq ansi-color-for-comint-mode t)
